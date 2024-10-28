948d1841726f9d3556473a8e3f92d6d2bff99f5d
stemil23 <stemil23@gmail.com>
2024-10-29 10:12:34 +1100

    test env variables

--- dbCloud.ts
+++ dbCloud.ts
@@ -2,8 +2,8 @@ import * as edgedb from "edgedb";
 import e from "$generated/index.ts";
 
 const client = edgedb.createHttpClient({
-    instanceName: Deno.env.get("EDGEDB_CLOUD_INSTANCE") || "",
-    secretKey: Deno.env.get("EDGEDB_CLOUD_KEY") || ""
+    instanceName: "nbwt1_eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlZGIuZC5hbGwiOnRydWUsImVkYi5pLmFsbCI6dHJ1ZSwiZWRiLnIuYWxsIjp0cnVlLCJpYXQiOjE3MzAwODA2MzMsImlzcyI6ImF3cy5lZGdlZGIuY2xvdWQiLCJqdGkiOiI4emotd0pUUEVlLVpGUU8wV3RIN0Z3Iiwic3ViIjoiRWdQbnNHbXNFZS16SENlenJ6UlBJUSJ9.mCS67pxX9hWq1jsOVROQIoNYGyvWHu1kV-Iv9oZxcns0yN6iPPSVjchtLen_ko8BkAtBWqcK1x8sgs7_er96eg",
+    secretKey: "stemil23/mydb"
 });
 
 export default client;
