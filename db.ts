3477f65a7299ddb492ba2a82f861ff0ec1b1c87c
stemil23 <stemil23@gmail.com>
2024-10-29 12:46:21 +1100

    DSN working with strict

--- db.ts
+++ db.ts
@@ -1,7 +1,11 @@
-import { createClient } from "edgedb";
+import { createHttpClient } from "edgedb";
 import e from "$generated/index.ts";
 
-const client = createClient();
+const client = createHttpClient({
+    instanceName: Deno.env.get("EDGEDB_DSN"),
+    secretKey: Deno.env.get("EDGEDB_SECRET_KEY"),
+    tlsSecurity: "insecure"
+  });
 
 export default client;
 export { e };
--- dbCloud.ts
+++ dbCloud.ts
@@ -2,8 +2,9 @@ import * as edgedb from "edgedb";
 import e from "$generated/index.ts";
 
 const client = edgedb.createHttpClient({
-  instanceName: Deno.env.get("EDGEDB_INSTANCE_NAME"),
-  secretKey: Deno.env.get("EDGEDB_SECRET_KEY"),
+  dsn: Deno.env.get("EDGEDB_DSN"),
+  secretKey: Deno.env.get("EDGEDB_CLOUD_KEY"),
+  tlsSecurity: "strict"
 });
 
 export default client;
