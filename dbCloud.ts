ab1fe2cb6ae6697c462a7a9f1a5965c85fc1e43c
stemil23 <stemil23@gmail.com>
2024-10-31 09:12:52 +1100

    Igor workaround

--- dbCloud.ts
+++ dbCloud.ts
@@ -1,9 +1,18 @@
 import * as edgedb from "edgedb";
 import e from "$generated/index.ts";
 
+// Workaround to get edgedb client library working on Deno Deploy
+if (!Deno.permissions.querySync) {
+    (Deno.permissions as unknown as Record<string, unknown>)["querySync"] = (
+      _pd: Deno.PermissionDescriptor,
+    ): { state: string } => ({ state: "granted" });
+  }
+  // End of workaround
+
 const client = edgedb.createHttpClient({
-    dsn: "edgedb://sql:buffy23@mydb--stemil23.c-78.i.aws.edgedb.cloud:5656/main?sslmode=require",
-    secretKey: Deno.env.get("EDGEDB_CLOUD_KEY") || ""
+    // dsn: "edgedb://sql:buffy23@mydb--stemil23.c-78.i.aws.edgedb.cloud:5656/main?sslmode=require",
+    dsn: Deno.env.get("EDGEDB_DSN"),
+    secretKey: Deno.env.get("EDGEDB_CLOUD_KEY")
 });
 
 export default client;
