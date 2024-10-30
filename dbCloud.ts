da30bb99aba88ca073fcc369f50fbc60ea716030
stemil23 <stemil23@gmail.com>
2024-10-30 12:49:38 +1100

    DSN in client

--- dbCloud.ts
+++ dbCloud.ts
@@ -2,7 +2,7 @@ import * as edgedb from "edgedb";
 import e from "$generated/index.ts";
 
 const client = edgedb.createHttpClient({
-    dsn: Deno.env.get("EDGEDB_DSN") || "",
+    dsn: "edgedb://sql:buffy23@mydb--stemil23.c-78.i.aws.edgedb.cloud:5656/main?sslmode=require",
     secretKey: Deno.env.get("EDGEDB_CLOUD_KEY") || ""
 });
 
