8076f6bb2e72a9df6095afa4d413dfc117b56df0
stemil23 <stemil23@gmail.com>
2024-11-01 17:09:19 +1100

    test client

--- utils/edgedb/client.ts
+++ utils/edgedb/client.ts
@@ -9,22 +9,11 @@ if (!Deno.permissions.querySync) {
   }
   // End of workaround
 
+const client = edgedb.createHttpClient({
+    // dsn: "edgedb://sql:buffy23@mydb--stemil23.c-78.i.aws.edgedb.cloud:5656/main?sslmode=require",
+    dsn: Deno.env.get("EDGEDB_DSN"),
+    secretKey: Deno.env.get("EDGEDB_CLOUD_KEY")
+});
 
-
-
-
-function createDbClient() {
-  // Check if we're using EdgeDB Cloud
-  const isCloud = Deno.env.get("EDGEDB_CLOUD_INSTANCE") && Deno.env.get("EDGEDB_CLOUD_KEY");
-  if (isCloud) {
-    return edgedb.createHttpClient({
-        dsn: Deno.env.get("EDGEDB_DSN"),
-        secretKey: Deno.env.get("EDGEDB_CLOUD_KEY")
-    });
-  }
-  // Default to local client
-  return edgedb.createClient();
-}
-const client = createDbClient();
 export default client;
 export { e };
